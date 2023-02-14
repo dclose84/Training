90fb5125a7184953076f0c858b19ba997cb0d9f7
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-02-13 20:01:01 -0800

    23-02-13_HomePM

--- vb/VBA - Kyle Pew Project 5.vb
+++ vb/VBA - Kyle Pew Project 5.vb
@@ -1,2 +1,107 @@
 VBA - Kyle Pew Project 4 (loops with logic)
 
+Sub LoopYearlyReport()
+    Dim ws As Worksheet
+    
+    For Each ws In Worksheets
+        Worksheets(ws.Name).Select
+        
+        InsertHeaders
+        FormatHeaders
+        AutomateTotalSUM
+        
+    Next ws
+End Sub
+
+
+
+
+
+
+Public Sub AutomateTotalSUM()
+    Dim lastCell As String
+    Dim ws As Worksheet
+
+    For Each ws In Worksheets
+        Worksheets(ws.Name).Select
+        
+        Range("F2").Select
+        
+        Selection.End(xlDown).Select
+        
+        lastCell = ActiveCell.Address(False, False)
+        
+        ActiveCell.Offset(1, 0).Select
+        
+        ActiveCell.Value = "=sum(F2:" & lastCell & ")"
+    Next ws
+End Sub
+
+Sub InsertHeaders()
+'
+' InsertHeaders Macro
+' Inserts a new row and add the list headers
+'
+
+'
+    Rows("1:1").Select
+    Selection.Insert Shift:=xlDown
+    Range("A1").Select
+    ActiveCell.FormulaR1C1 = "Division"
+    Range("B1").Select
+    ActiveCell.FormulaR1C1 = "Category"
+    Range("C1").Select
+    ActiveCell.FormulaR1C1 = "Jan"
+    Range("D1").Select
+    ActiveCell.FormulaR1C1 = "Feb"
+    Range("E1").Select
+    ActiveCell.FormulaR1C1 = "Mar"
+    Range("F1").Select
+    ActiveCell.FormulaR1C1 = "Total"
+    Range("A2").Select
+End Sub
+
+
+Sub FormatHeaders()
+'
+' FormatHeaders Macro
+' Formats list headers and list content
+'
+
+'
+    Range("A1:F1").Select
+    Selection.Font.Bold = True
+    With Selection.Interior
+        .Pattern = xlSolid
+        .PatternColorIndex = xlAutomatic
+        .ThemeColor = xlThemeColorAccent1
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+    With Selection.Font
+        .ThemeColor = xlThemeColorDark1
+        .TintAndShade = 0
+    End With
+    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
+    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
+    Selection.Borders(xlEdgeLeft).LineStyle = xlNone
+    Selection.Borders(xlEdgeTop).LineStyle = xlNone
+    With Selection.Borders(xlEdgeBottom)
+        .LineStyle = xlContinuous
+        .ColorIndex = 0
+        .TintAndShade = 0
+        .Weight = xlMedium
+    End With
+    Selection.Borders(xlEdgeRight).LineStyle = xlNone
+    Selection.Borders(xlInsideVertical).LineStyle = xlNone
+    Selection.Borders(xlInsideHorizontal).LineStyle = xlNone
+    Selection.Font.Size = 12
+    Range("C2").Select
+    Range(Selection, Selection.End(xlDown)).Select
+    Range(Selection, Selection.End(xlToRight)).Select
+    Selection.Style = "Currency"
+    Columns("B:F").Select
+    Columns("B:F").EntireColumn.AutoFit
+    Range("A2").Select
+End Sub
+

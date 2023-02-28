b4c04d888334e6776cadf5415726fad72ddd8c50
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-02-28 15:23:19 -0800

    23-02-28_WorkPM

--- vb/VBA - Kyle Pew Project 6.vb
+++ vb/VBA - Kyle Pew Project 6.vb
@@ -1,2 +1,17 @@
 VBA - Kyle Pew Project 6 (user forms)
 
+Private Sub UserForm_Click()
+    MsgBox ("Hello World!")
+End Sub
+
+Private Sub UserForm_Initialize()
+   Dim i As Integer
+   
+   i = 1
+   
+   Do While i <= Worksheets.Count
+        Me.cboWhichSheet.AddItem Worksheets(i).Name
+        
+        i = i + 1
+   Loop
+End Sub

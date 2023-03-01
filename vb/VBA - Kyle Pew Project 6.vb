d42097900d57a58b19ca141798db5473169c4de2
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-03-01 13:53:37 -0800

    23-03-01_Work1

--- vb/VBA - Kyle Pew Project 6.vb
+++ vb/VBA - Kyle Pew Project 6.vb
@@ -1,5 +1,15 @@
 VBA - Kyle Pew Project 6 (user forms)
 
+Private Sub btnAddWorksheet_Click()
+    Worksheets.Add before:=Worksheets(1)
+    
+    ActiveSheet.Name = InputBox("Please enter a new worksheet name")
+End Sub
+
+Private Sub cboWhichSheet_Change()
+    Worksheets(Me.cboWhichSheet.Value).Select
+End Sub
+
 Private Sub UserForm_Click()
     MsgBox ("Hello World!")
 End Sub

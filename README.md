beeef01aacc4474efdb9e63d108d3a4932d69f1a
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-03-14 11:53:17 -0700

    23-03-14_R1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Close-Codes
 # My-**training**-repository
-Last updated 2023-03-13.
+Last updated 2023-03-14.
--- vb/VBA - Kyle Pew Project 7.vb
+++ vb/VBA - Kyle Pew Project 7.vb
@@ -4,19 +4,29 @@ Public Sub ImportTextFile()
     Dim TextFile As Workbook
     ' () denotes an array
     Dim OpenFiles() As Variant
+    Dim i As Integer
     
     OpenFiles = Application.GetOpenFilename(Title:="Select File(s) to Import", MultiSelect:=True)
     
-    ' set makes this an excel object
-    Set TextFile = Workbooks.Open(OpenFiles(1))
+    ' create the loop
+    For i = 1 To Application.CountA(OpenFiles)
     
-    ' Copy the newly created workbook of the import into the existing original workbook
-    TextFile.Sheets(1).Range("A1").CurrentRegion.Copy
-    ' demo was saying Workbooks(1), but since PERSONAL worksheet exists, that is Workbooks(1), had to change to Workbooks(2)
-    Workbooks(2).Activate
-    ActiveSheet.Paste
-    
-    ' close out the text file that was created
-    TextFile.Close
-    
+        ' set makes this an excel object
+        Set TextFile = Workbooks.Open(OpenFiles(i))
+        
+        ' Copy the newly created workbook of the import into the existing original workbook
+        TextFile.Sheets(1).Range("A1").CurrentRegion.Copy
+        ' demo was saying Workbooks(1), but since PERSONAL worksheet exists, that is Workbooks(1), had to change to Workbooks(2)
+        Workbooks(2).Activate
+        Workbooks(2).Worksheets.Add
+        ActiveSheet.Paste
+        ' rename the newly created sheet
+        ActiveSheet.Name = TextFile.Name
+        
+        ' clear the clipboard error
+        Application.CutCopyMode = False
+        
+        ' close out the text file that was created
+        TextFile.Close
+    Next i
 End Sub

d9bd3535bf10416b2aa6ab8ff7374cdde5498330
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-03-28 12:04:14 -0700

    23-03-28_R1

---
+++ py/Angela Yu 100 Days/Day 2 Project.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#String "", [] subscript -> will index/position , 0 = first letter in the string. Below will print the H in Hello
+print("Hello"[4])
+
+# this will not show as a number, but as a string, ie. 123345
+print("123" + "345")
+
+#Integer are numbers, _ are equivalent to commas separating numbers, will print without them
+print(123 + 345)
+print(123_456_789)
+
+#Float , decimal numbers
+3.14159
+
+#Boolean, true or false
+True
+False

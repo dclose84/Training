cc2408deaf3a5f829cc0018f88fcf45f75481b18
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-04-07 16:20:14 -0700

    23-04-07_R2

---
+++ py/API Info.py
@@ -0,0 +1,11 @@
+# Python Spotify API
+
+# Oauth = Open Authorization, open standard for access delegation
+# API = Application Programming Interface
+# Authentication = I prove that I am who I say I am , Username/Password, Selenium
+# Authorization = I prove that someone else told me I could access something, Oauth
+
+# Scope = What I get access to, ex. user-playlists, email
+# Client ID = Unique ID for your app, ex. username
+# Redirect uri = After user approves, where do we send them back to? 
+# Client Secret = Private secret only you and the server know, ex. password

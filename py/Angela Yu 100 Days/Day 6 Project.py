198deb6154f1ae69b494cff54657163c181a0481
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-05-03 12:58:32 -0700

    23-05-03_R1

--- py/Angela Yu 100 Days/Day 6 Project.py
+++ py/Angela Yu 100 Days/Day 6 Project.py
@@ -39,3 +39,29 @@ def hurdle():
 #instead of calling hurdle() 6 times, make a FOR loop with RANGE
 for step in range(6):
     hurdle()
+
+#62 While Loops
+# While robot is plugged into wall, move forward
+# For item in list_of_items: Do something to each Item
+# For number in range(a,b): print(number)
+
+while something_is_true:
+    Do something repeatedly
+    
+number_of_hurdles = 6
+while number_of_hurdles >0:
+    jump()
+    number_of_hurdles -= 1
+    
+while not at_goal():
+    hurdle()
+    
+#or 
+while at_goal() != True:
+    hurdle()
+    
+# For loops are often better for multiple prints, while loops work better when not having to perform multiple prints
+# For loops have a determined number of iterations, while is logic based on unknown iterations
+# While loops can be infinite and is an issue (infinite loops)
+
+#63 While loops advanced

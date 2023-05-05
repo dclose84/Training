381612f888f9198628cd4b8a717ed5a0b306c02c
dclose84 <97866961+dclose84@users.noreply.github.com>
2023-05-05 16:13:31 -0700

    23-05-05_R1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Close-Codes
 # My-**training**-repository
-Last updated 2023-05-04.
+Last updated 2023-05-05.
--- py/Angela Yu 100 Days/Day 6 Project.py
+++ py/Angela Yu 100 Days/Day 6 Project.py
@@ -86,3 +86,50 @@ while at_goal() != True:
         scale()
         
 #64 Hurdles Challenge using While Loops
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def scale():
+    turn_right()
+    move()
+
+while at_goal() != True:
+    if wall_in_front() == True:
+        if right_is_clear() == True:
+            scale()
+        else:
+            turn_left()
+    elif wall_on_right() == True:
+        move()
+    elif front_is_clear() == True:
+        if right_is_clear() == True:
+            scale()
+        else:
+            move()
+
+#suggested
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while at_goal() != True:
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+#65 Final Project: Escaping the Maze
